106f82ac4bbfd7279d6f7b403aaf4d6aca3a2353
manulmax <49731724+ManulMax@users.noreply.github.com>
2019-06-23 00:35:49 +0530

    Add files via upload

---
+++ serial.read/serial.read.ino
@@ -0,0 +1,27 @@
+int input=0;
+int led=13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+      
+    input= Serial.read();
+    
+      
+        } 
+    if(input==49){
+      digitalWrite(led,HIGH);
+      delay(100);
+      digitalWrite(led,LOW);
+      delay(100);
+      }
+    if(input==50){
+      digitalWrite(led,HIGH);
+      delay(50);
+      digitalWrite(led,LOW);
+      delay(50); 
+      }    
+}
